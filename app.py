91c5fc711cc01d1ed8a1fba076061ba5e6ed436a
ytchuang1018 <41098854+ytchuang1018@users.noreply.github.com>
2022-01-09 16:41:57 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,make_response, send_from_directory
+from flask import Flask,jsonify, make_response, send_from_directory
 import os
 
 app = Flask(__name__)
@@ -9,6 +9,11 @@ def index():
 
 @app.route('/user', methods=["GET"])
 def user():
+    data={"name":"test2", "age":"20", "content":"測試"}
+    return jsonify(data)
+
+@app.route('/user_download', methods=["GET"])
+def user_download():
     response = make_response(send_from_directory(os.path.dirname(__file__), 'test.json', as_attachment=True))
     response.headers["Content-Disposition"] = "attachment; filename={}".format('user.json')
     return response
